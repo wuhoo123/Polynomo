164ec5143d2226d47c6edb3f1500cf87a3d9ac95
wuhoo123 <wzwu23@students.shrewsbury.k12.ma.us>
2021-12-18 09:12:19 -0500

    Added new tab format

---
+++ Polynomo/Controller/FeedViewController.swift
@@ -0,0 +1,16 @@
+//
+//  FeedViewController.swift
+//  Polynomo
+//
+//  Created by William Wu on 12/11/21.
+//
+
+import Foundation
+import UIKit
+
+class FeedViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
